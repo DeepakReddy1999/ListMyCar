95836983e9aaf13ced49d2b22480f4a27fbae65d
Sai Deepak Reddy Y <ysdreddy1999@gmail.com>
2024-09-18 21:22:31 -0700

    Spring Boot Initilization

---
+++ Backend/ListMyCar/src/main/java/com/ListMyCarBackend/ListMyCar/ListMyCarApplication.java
@@ -0,0 +1,13 @@
+package com.ListMyCarBackend.ListMyCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListMyCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListMyCarApplication.class, args);
+	}
+
+}
---
+++ Backend/ListMyCar/src/test/java/com/ListMyCarBackend/ListMyCar/ListMyCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ListMyCarBackend.ListMyCar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListMyCarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
